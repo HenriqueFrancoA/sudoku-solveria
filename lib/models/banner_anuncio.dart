2afeb271d74b2bfca18cff6b5a7ce2419b8a9b4d
Henrique Franco <henriquefrancoaraujo@gmail.com>
2023-11-17 10:12:39 -0300

    banner

--- lib/models/banner_anuncio.dart
+++
@@ -1,8 +0,0 @@
-class BannerAnuncio {
-  static String id = 'ca-app-pub-4824022930012497/8213200469';
-  static String testeId = 'ca-app-pub-3940256099942544/6300978111';
-  static String idEsticado = 'ca-app-pub-4824022930012497/9929870147';
-  static String testeIdEsticado = 'ca-app-pub-3940256099942544/1033173712';
-  static String idPremiado = 'ca-app-pub-4824022930012497/8871676014';
-  static String testeIdPremiado = 'ca-app-pub-3940256099942544/5224354917';
-}
